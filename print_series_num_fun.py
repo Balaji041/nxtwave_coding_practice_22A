6c1af69e804e7557234a9e02693469ee566fe4aa
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-23 22:54:14 +0530

    Create print_series_num_fun.py

---
+++ print_series_num_fun.py
@@ -0,0 +1,19 @@
+def print_numbers(number):
+
+    # complete this function
+    for i in range(1,number+1):
+        print(i)
+
+number = int(input())
+
+# call the print_numbers function
+print_numbers(number)
+
+"""
+input:4
+output:
+1
+2
+3
+4
+"""
