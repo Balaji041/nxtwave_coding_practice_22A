2afde5f43760da6f3eb05ca4a3000011337e911a
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-23 22:36:45 +0530

    Create basic_fun.py

---
+++ basic_fun.py
@@ -0,0 +1,17 @@
+def print_arg(string):
+    
+    # complete this function
+    print(string)
+    
+
+
+string = input()
+
+# call the print_arg function
+print_arg(string)
+
+
+"""
+input:balaji
+output:balaji
+"""
