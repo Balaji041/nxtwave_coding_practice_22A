8c08c11a6f88db45572453cffb01abd105d05d48
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-23 22:47:55 +0530

    Create between_200_500 _fun.py

---
+++ between_200_500 _fun.py
@@ -0,0 +1,17 @@
+def is_between_200_and_500(number):
+
+    #complete this function
+    if number>200 and number<500:
+        print("Yes")
+    else:
+        print("No")
+
+number = int(input())
+
+#call the is_between_200_and_500 function
+is_between_200_and_500(number)
+
+"""
+input:280
+output:yes
+"""
