e24933e75a44b4ab41c3f74878d32e1981f52795
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-23 22:44:54 +0530

    Create divisble_by7_fun.py

---
+++ divisble_by7_fun.py
@@ -0,0 +1,15 @@
+def divisible_by_seven(arg_1):
+    # Write your code here
+    if arg_1%7==0:
+        print("True")
+    else:
+        print("False")
+        
+
+n = int(input())
+divisible_by_seven(n)
+
+"""
+input:14
+output:true
+"""
